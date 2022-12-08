efad32e14d40701033e6a151540dc023d674a91a
Maxim Severyanov <zandreks@gmail.com>
2022-12-08 23:50:08 +0600

    done base

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeConfig.getThemeConfig(isDarkMode, context),
       key: ValueKey('${context.locale}'),
-      title: 'e-Ve pro',
+      title: 'View project',
       scaffoldMessengerKey: snackBarKey,
       initialRoute: '/',
       routes: routesRoot,
